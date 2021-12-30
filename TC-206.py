ee6d154a016ec3b3691619a55b7b744b08776d91
nhp-0304 <nguyenhuuphuc3421@gmail.com>
2021-12-30 16:50:09 +0700

    Danh muc san pham

---
+++ TC-206.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path='venv/chromedriver')
+driver.get('https://moji.vn/')
+
+driver.set_window_size(1280,680)
+driver.find_element(By.LINK_TEXT, 'TẤT CẢ').click()
+driver.find_element(By.CSS_SELECTOR, 'li:nth-child(9) > .drop').click()
+
+articles = driver.find_elements(By.ID, 'c-158205')
+for item in articles:
+    try:
+        title = item.find_element(By.CSS_SELECTOR, 'li')
+        link = item.find_element(By.CSS_SELECTOR, 'li>a')
+        print(title.text)
+        print(link.get_attribute('href'))
+        print('__________')
+    except NoSuchElementException:
+        pass
+
+driver.close()
